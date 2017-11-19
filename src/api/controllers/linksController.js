3667e7897b2301d5c7480a7c1aebc65a419c1cf1
Uladzislau Sazanovich <zerogerc@gmail.com>
2017-11-19 11:56:02 +0300

    default headers

--- src/api/controllers/linksController.js
+++ src/api/controllers/linksController.js
@@ -3,16 +3,9 @@ import type {$Request, $Response, } from 'express'
 
 import Link from 'api/models/linksModel'
 
-function appendHeader(res: $Response) {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:8000')
-  res.header('Access-Control-Allow-Credentials', 'true')
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-}
-
 exports.listAllLinks = (req: $Request, res: $Response) => {
   Link.find({}, (err: string, task: string) => {
     if (err) { res.send(err) }
-    appendHeader(res)
     res.json(task)
   })
 }
@@ -21,7 +14,6 @@ exports.createLink = (req: $Request, res: $Response) => {
   let newLink = new Link(req.body)
   newLink.save((err: string, task: string) => {
     if (err) { res.send(err) }
-    appendHeader(res)
     res.json(task)
   })
 }
@@ -29,7 +21,6 @@ exports.createLink = (req: $Request, res: $Response) => {
 exports.readLink = (req: $Request, res: $Response) => {
   Link.findById(req.params.linkId, (err: string, task: string) => {
     if (err) { res.send(err) }
-    appendHeader(res)
     res.json(task)
   })
 }
--- src/index.js
+++ src/index.js
@@ -5,13 +5,18 @@ import bodyParser from 'body-parser'
 
 import routes from 'api/routes/linksRoutes'
 
-// greetings();
-
 const app = express()
 const port = process.env.PORT || 3000
 
 mongoose.Promise = global.Promise
 mongoose.connect('mongodb://localhost/LinksTempDB')
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8000')
+  res.setHeader('Access-Control-Allow-Credentials', 'true')
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next()
+})
 
 app.use(bodyParser.urlencoded({ extended: true, }))
 app.use(bodyParser.json())
